417d9f61393ef88a606f2636a6f479a774c9ef05
Simone Gaiarin <simgunz@gmail.com>
2021-11-02 09:43:42 +0100

    Init brain brew project

--- build-deck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd deck
-cat collection-dump.sql | sqlite3 collection.anki2
-find ./ -type f ! -name "collection-dump.sql" | xargs zip -j "../Italian IPA.apkg"
-rm collection.anki2
-cd ..
